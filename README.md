01615df02185f905eedbd2467322dd2141401003
Tasnádi Zoltán <tasnadi98@gmail.com>
2023-03-07 18:52:46 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # julia_wei_format
 A small package that I needed for multiple Pluto.jl projects and wanted to access easily.
+
+Doesn't support:
+
+  * Loading multiple graphs from the same file
+
+  * Writing graphs in this format
+
+  * Assigning names to graphs
--- src/WeightedEdgeListFormat.jl
+++ src/WeightedEdgeListFormat.jl
@@ -1,5 +1,29 @@
 module WeightedEdgeListFormat
 
-greet() = print("Hello World!")
+  struct WELFormat <: Graphs.AbstractGraphFormat
+    delim
+    WELFormat() = new(",")
+    WELFormat(delim) = new(delim)
+  end
+
+  function Graphs.loadgraph(io::IO, gname::String, f::WELFormat)
+    g, _ = read_edge_list_weighted(io; delim=f.delim)
+
+    g
+  end
+
+  Graphs.loadgraphs(io::IO, f::WELFormat) = loadgraph(io, "...", f)
+
+  function read_edge_list_weighted(filename; delim=" ")
+    csv = CSV.read(filename, DataFrame; header=false, delim=delim)
+    if minimum(csv[:, 1]) == 0 || minimum(csv[:, 2]) == 0
+      csv .+= 1
+    end
+    labels = unique(sort(vcat(csv[:, 1], csv[:, 2])))
+    g = SimpleWeightedGraph(csv[:, 1], csv[:, 2], csv[:, 3]; combine=max)
+
+    g, labels
+  end
+
 
 end # module WeightedEdgeListFormat
